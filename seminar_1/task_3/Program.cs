157894dc5a74341e114a10c5aca5522ae2f44392
боков дмитрий <dbokov@rambler.ru>
2022-09-03 10:21:22 +0300

    DZ_Seminar_1

---
+++ seminar_1/task_3/Program.cs
@@ -0,0 +1,43 @@
+﻿// 3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+
+Console.WriteLine("Введите числом день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+{
+
+if (number == 1)
+    {
+        Console.WriteLine("День недели - Понедельник");
+    }
+if (number == 2)
+    {
+        Console.WriteLine("День недели - Вторник");
+    }
+if (number == 3)
+    {
+        Console.WriteLine("День недели - Среда");
+    }
+if (number == 4)
+    {
+        Console.WriteLine("День недели - Четверг");
+    }
+if (number == 5)
+    {
+        Console.WriteLine("День недели - Пятница");
+    }
+if (number == 6)
+    {
+        Console.WriteLine("День недели - Суббота");
+    }
+if (number == 7)
+    {
+        Console.WriteLine("День недели - Воскресенье");
+    }
+
+if (number < 1 || number > 7) 
+    {
+        Console.WriteLine("В неделе 7 дней!!!))");
+    }
+}
---
+++ seminar_1/task_4/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 4: Напишите программу, которая принимает на 
+// вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a == b && b == c)
+    {
+    Console.WriteLine("Все числа максимальные = " + a);
+    }
+else if (a == b)
+    {
+    Console.WriteLine("Максимальные числа №1 и №2 = " + a);
+    }
+else if (a == c)
+    {
+    Console.WriteLine("Максимальные числа №1 и №3 = " + a);
+    }
+else if (b == c)
+    {
+    Console.WriteLine("Максимальные числа №2 и №3 = " + b);
+    }
+else if (a >= b && a >= c)
+    {
+    Console.WriteLine("Максимальное число №1 = " + a);
+    }
+else if (b >= a && b >= c)
+    {
+    Console.WriteLine("Максимальное число №2 = " + b);
+    }
+else if (c >= a && c >= b)
+    {
+    Console.WriteLine("Максимальное число №3 = " + c);
+    }

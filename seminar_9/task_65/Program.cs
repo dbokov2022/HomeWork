8f6371a7ba075245723ec708976c8b3092f7ef76
боков дмитрий <dbokov@rambler.ru>
2022-09-28 10:57:35 +0300

    DZ_Seminar_9

---
+++ seminar_9/task_65/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 65: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void PrintMtoN(int m, int n)
+{
+    if(n < m)
+    {
+        if (m == n - 1)
+        {
+            return;
+        }
+        Console.Write(m + " ");
+        PrintMtoN(m - 1, n);
+    }
+    else if(n > m)
+    {
+        if(n == m - 1)
+        {
+            return;
+        }
+        PrintMtoN(m, n - 1);
+        Console.Write(n + " ");
+    }
+    else if(n == m) Console.Write(n + " ");
+}
+
+int M = ReadNumber("Введите M");
+int N = ReadNumber("Введите N");
+
+PrintMtoN(M, N);
---
+++ seminar_9/task_66/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int ReadNumber(string message)
+{
+  Console.WriteLine(message);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int SummMToN(int m, int n)
+{
+  int sum = 0;
+  if (n == m)
+  {
+    return n;
+  }
+  else if (n > m)
+  {
+    sum = n + SummMToN(m, n - 1);
+    return sum;
+  }
+  else return -1;
+}
+
+int m = ReadNumber("Введите M: ");
+int n = ReadNumber("Введите N: ");
+
+if (n > m) Console.Write(SummMToN(m, n));
+else if (n < m) Console.Write(SummMToN(n, m));
+else Console.Write(n);
+
+
+
+
